3c551376b1695e6070c660e1fbeb92735f6b4008
Elder Vivot <eldervivot@gmail.com>
2023-10-25 08:16:24 -0300

    feat: implement option to reprocess

--- src/handler.py
+++ src/handler.py
@@ -22,14 +22,24 @@ def main(event, context):
     client = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name=REGION_NAME)
 
     for item in event.get("Records"):
-        print(item)
-        s3 = item.get("s3")
-        bucket = s3.get("bucket").get("name")
-        key = s3.get("object").get("key")
-        extension = key.split('.')[1]
-        fileObj = client.get_object(Bucket=bucket, Key=key)
-        fileContent = fileObj['Body'].read()
-        fileBytesIO = io.BytesIO(fileContent)
+        eventSource = item.get('eventSource')
+        if eventSource == 'aws:dynamodb':
+            url = item.get("dynamodb").get('NewImage').get('url').get('S')
+            urlSplit = url.split('/')
+            bucket = urlSplit[2].split('.')[0]
+            key = '/'.join(urlSplit[3:])
+            extension = key.split('.')[1]
+            fileObj = client.get_object(Bucket=bucket, Key=key)
+            fileContent = fileObj['Body'].read()
+            fileBytesIO = io.BytesIO(fileContent)
+        else:
+            s3 = item.get("s3")
+            bucket = s3.get("bucket").get("name")
+            key = s3.get("object").get("key")
+            extension = key.split('.')[1]
+            fileObj = client.get_object(Bucket=bucket, Key=key)
+            fileContent = fileObj['Body'].read()
+            fileBytesIO = io.BytesIO(fileContent)
 
         if extension in ('xlsx', 'xls'):
             data = readExcelPandas(fileBytesIO)
